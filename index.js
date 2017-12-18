faa2a21a42e6118a5d293960d0d8395ffdb5287b
Sin Cheung Ko <scko823@gmail.com>
2017-12-17 20:07:13 -0700

    misc lint fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-exports.getAllTweets = (event, context, callback) => {
-  const GetAllTweets = require('./lib/get-all-tweets');
-  GetAllTweets();
+exports.getAllTweets = (/* event, context, callback */) => {
+	const GetAllTweets = require('./lib/get-all-tweets');
+	GetAllTweets();
 };
 
 // const GetAllTweets = require('./lib/get-all-tweets');
